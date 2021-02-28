1f6495896cfa838b996502308635dc2f3eebdcef
Davydov <davydovvip@gmail.com>
2021-02-28 23:11:21 +0500

    init project

---
+++ src/main/java/com/davydov/DavydovApplication.java
@@ -0,0 +1,12 @@
+package com.davydov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavydovApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DavydovApplication.class, args);
+  }
+}
